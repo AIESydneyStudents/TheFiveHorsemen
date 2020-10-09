e2b895f3870198324423a635f967c92edd9bc8e6
Jacob Cooper <s200503@students.aie.edu.au>
2020-10-09 15:55:58 +1100

    Test (test) : #8080 : Adding test.

---
+++ project/Assets/Scripts/Player/ControllerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+    enum availableControllers
+    {
+        Controller1 = 1,
+        Controller2,
+        Controller3,
+        Controller4
+    };
+
+    // The reason I serialized a public string is because it will put it in the editor even if its inherited.
+    [SerializeField] private availableControllers selectedController = availableControllers.Controller1;
+
+    public string controllerInput { get; private set; }
+    public bool controllerExists { get; private set; }
+
+    public virtual void Start()
+    {
+        controllerInput = selectedController.ToString();
+        controllerExists = ControllerExists();
+
+        Debug.Log(controllerInput + " : " + controllerExists);
+    }
+
+    public bool ControllerExists()
+    {
+        return (Input.GetJoystickNames().Length + 1) > (int)selectedController;
+    }
+
+    public float GetHorizontalAxis()
+    {
+        return (controllerExists) ? Input.GetAxis(controllerInput + "_Horizontal") : 0;
+    }
+
+    public float GetVerticalAxis()
+    {
+        return (controllerExists) ? Input.GetAxis(controllerInput + "_Vertical") : 0;
+    }
+}
---
+++ project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : ControllerInput
+{
+    private Vector3 moveDirection = Vector3.zero;
+    private float xPos = 0f;
+    private float yPos = 0f;
+
+    private CharacterController controller;
+
+    [SerializeField] private float moveSpeed = 6.0F;
+
+    public override void Start()
+    {
+        base.Start();
+
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Move()
+    {
+        xPos += GetHorizontalAxis();
+        yPos += GetVerticalAxis();
+
+        transform.Rotate(0, xPos, 0);
+
+        moveDirection = transform.forward;
+        moveDirection = moveDirection * yPos;
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        xPos = 0;
+        yPos = 0;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+}
