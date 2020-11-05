35ec04ac7f8792147d86204935206779e3031be6
Aaron <A.Fuser04@gmail.com>
2020-11-05 16:58:07 +1100

    Scripts and level dev
    Level iterated upon, deathbox script added.

--- project/Assets/Scripts/Menu/MenuButton.cs
+++ project/Assets/Scripts/Menu/MenuButton.cs
@@ -6,6 +6,7 @@ public class MenuButton : MonoBehaviour
 {
     private Vector3 startPos;
     private bool finished = false;
+    public int brickValue;
 
     [System.Serializable]
     public struct dissapear
@@ -47,6 +48,8 @@ public class MenuButton : MonoBehaviour
 
     void FixedUpdate()
     {
+        
+
         if (!joystick.Clicking() && !finished)
         {
             if (ButtonHover())
@@ -89,7 +92,7 @@ public class MenuButton : MonoBehaviour
                 }
             }
 
-            if (ready == dissapears.Length)
+            if (brickValue == 2)
             {
                 //Camera.main.transform.position = cameraPos;
                 //Camera.main.transform.position = cameraAng;
@@ -99,6 +102,16 @@ public class MenuButton : MonoBehaviour
                 int level = Random.Range(1, sceneTo + 1);
                 UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(level);
             }
+
+            if (brickValue == 1)
+            {
+                Application.Quit();
+            }
+
+            //if (Brickvalue == 3)
+            //{
+
+            //}
         }
     }
 }
---
+++ project/Assets/Scripts/Player/DeathBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.Die(3);
+        }
+    }
+       
+}
